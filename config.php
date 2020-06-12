51e64a3ccc9b0ce489f63614a893d7e663486b84
DAB <dab@dab.la>
2020-06-12 10:20:16 -0400

    Refactored core compiled js files

--- config.php
+++ config.php
@@ -5,11 +5,13 @@ return [
         'module' => 'AutoPop',
         'version' => '1.0',
     ],
-    'js' => [
-        // Module Name
-        'AutoPop' => [
-            // Source file => Dest file
-            'AutoPop.js' => 'site.min.js',
-        ]
+    'compiler' => [
+        'js' => [
+            // Module Name
+            'lightningsdk/autopop' => [
+                // Source file => Dest file
+                'AutoPop.js' => 'site.min.js',
+            ]
+        ],
     ],
 ];
